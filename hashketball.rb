7069012009dd52f9190665027aa4f2f967202f16
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-24 01:27:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,6 +194,7 @@ end
 # Returns array of team names
 def team_names
   array = []
+  game_hash.select do |k,v
   array << game_hash[:home][:team_name]
   array << game_hash[:away][:team_name]
 end
