beee1b6780bf1071d8fc7743db5c31a8a8a4d177
Arthur Hwang <arthur.hwang@yale.edu>
2020-05-24 01:30:03 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,6 @@
+# Require 
+require 'pry'
+
 # Write your code below game_hash
 def game_hash
   {
@@ -129,111 +132,6 @@ end
 # Write code here
 
 # Helper methods
-# Returns if player team is away or home
-def player_side(name)
-  home_players = ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]
-  away_players = ["Jeff Adrien", "Bismack Biyombo", "DeSagna Diop", "Ben Gordon", "Kemba Walker"]
-  if home_players.include? name
-    p :home
-  elsif away_players.include? name
-    p :away
-  else
-    p nil
-  end
-end
-
-# Returns array index of player
-def player_index(name)
-  case name
-    when "Alan Anderson", "Jeff Adrien"
-      p 0 
-    when "Reggie Evans", "Bismack Biyombo"
-      p 1 
-    when "Brook Lopez", "DeSagna Diop"
-      p 2
-    when "Mason Plumlee", "Ben Gordon"
-      p 3
-    when "Jason Terry", "Kemba Walker"
-      p 4 
-    else
-      p nil 
-end
-end
-
-# Returns whether team name is home or away
-def team_side(team)
-  case team
-    when "Brooklyn Nets"
-      :home
-    when "Charlotte Hornets"
-      :away
-    else
-      nil
-  end
-end
-
-# Returns player's rebound stat
-def rebounds(name)
-  side = player_side(name)
-  index = player_index(name)
-  game_hash[side][:players][index][:rebounds]
-end
-
-# Implementation methods
-
-# Returns points scored by player
-def num_points_scored(name)
-  side = player_side(name)
-  index = player_index(name)
-  game_hash[side][:players][index][:points]
-end
-
-# Returns shoe size of the player
-def shoe_size(name)
-  side = player_side(name)
-  index = player_index(name)
-  game_hash[side][:players][index][:shoe]
-end
-
-# Returns array of team's colors
-def team_colors(team)
-  side = team_side(team)
-  game_hash[side][:colors]
-end
-
-# Returns array of team names
-def team_names
-  array = []
-  array << game_hash[:home][:team_name]
-  array << game_hash[:away][:team_name]
-end
-
-# Returns array of player numbers, given team name
-def player_numbers(team)
-  side = team_side(team)
-  numbers = []
-  counter = 0
-  while game_hash[side][:players][counter] do
-    numbers << game_hash[side][:players][counter][:number]
-    counter += 1
-end
-  numbers
-end
-
-# Returns hash of player's stats, given player's name
-def player_stats(name)
-  side = player_side(name)
-  index = player_index(name)
-  game_hash[side][:players][index]
-end
-
-# Returns number of rebounds associated with player that has largest shoe size
-def big_shoe_rebounds
-# Identify player's name with largest shoe size
-  big_foot = find_player_name(home_shoe_max)
-# Return that player's number of rebounds
-  rebounds(big_foot)
-end
 
 # Creates an separate array of all players
 def join_teams
@@ -250,4 +148,73 @@ end
 # Finds player name, given hash
 def find_player_name(hash)
   hash[:player_name]
+end
+
+# Returns team side, given team name
+def find_team_side(team)
+  hash = game_hash.select do |k,v|
+    v[:team_name] == team
+  end
+  hash.keys[0]
+end
+
+# Implementation methods
+
+# Returns hash of player's stats, given player's name.
+# Moved this method to the beginning to use with other following methods.
+
+def player_stats(name)
+  result = join_teams.select do |player|
+    player[:player_name] == name
+end
+  result[0]
+end
+
+
+# Returns points scored by player
+def num_points_scored(name)
+  hash = player_stats(name)
+  hash[:points]
+end
+
+
+# Returns shoe size of the player
+def shoe_size(name)
+  hash = player_stats(name)
+  hash[:shoe]
+end
+
+
+# Returns array of team's colors
+def team_colors(team)
+  side = find_team_side(team)
+  game_hash[side][:colors]
+end
+
+# Returns array of team names
+def team_names
+  array = []
+  array << game_hash[:home][:team_name]
+  array << game_hash[:away][:team_name]
+end
+
+# Returns array of player numbers, given team name
+def player_numbers(team)
+  side = find_team_side(team)
+  numbers = []
+  counter = 0
+  while game_hash[side][:players][counter] do
+    numbers << game_hash[side][:players][counter][:number]
+    counter += 1
+end
+  numbers
+end
+
+# Returns number of rebounds associated with player that has largest shoe size
+def big_shoe_rebounds
+# Identify player's name with largest shoe size
+  big_foot = find_player_name(home_shoe_max)
+# Return that player's number of rebounds
+  hash = player_stats(big_foot)
+  hash[:rebounds]
 end
